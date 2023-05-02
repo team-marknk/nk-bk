bd5e33a8ed952ce00eeb6d5d72d9697a919d716e
Hoshi0420 <kpcloud9@gmail.com>
2023-05-02 15:37:21 +0900

    feat: Define game()
    - Player have 10 chances.

--- bullcow.py
+++ bullcow.py
@@ -1,14 +1,41 @@
 import random
 
-q_num = ""
+def game():
 
-for i in range(4):
-    q_num += str(random.randint(0,9))
+    q_num = ""
 
-a_num = int(input('Input 4 numbers: '))
+    for i in range(4):
+        q_num += str(random.randint(0,9))
 
-if (len(str(a_num)) <= 3) | (len(str(a_num)) >= 5):
-    print('4자리 숫자를 입력해주세요.')
+    is_right = 0
+    count = 0
 
-print(a_num)
+    while is_right==0:
+
+        a_num = str(input('Input 4 numbers: '))
+
+        bull = 0
+        cow = 0
+        if count == 10:
+            print('10회안에 맞추지 못하셨습니다.')
+            exit()
+        if q_num == a_num:
+            print('4 bulls!!, Congratulation!!!')
+            is_right = 1
+        else:
+            for a in a_num:
+                if a in q_num:
+                    if q_num.index(a) == a_num.index(a):
+                        bull += 1
+                    elif q_num.index(a) != a_num.index(a):
+                        cow += 1
+
+            print(f'{bull} bulls, {cow} cows')
+            count += 1
+
+        if count == 10:
+            print('10회안에 맞추지 못하셨습니다.')
+            exit()
+
+game()
 
