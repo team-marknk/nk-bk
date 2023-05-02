a22c30780c325074ccfee15831b0007d6359c782
Hoshi0420 <kpcloud9@gmail.com>
2023-05-02 14:46:40 +0900

    feat: Create Input number
    - 입력 숫자가 3자리이하거나 5자리 이상일 경우 메세지 출력

--- bullcow.py
+++ bullcow.py
@@ -5,4 +5,10 @@ q_num = ""
 for i in range(4):
     q_num += str(random.randint(0,9))
 
-print(q_num)
+a_num = int(input('Input 4 numbers: '))
+
+if (len(str(a_num)) <= 3) | (len(str(a_num)) >= 5):
+    print('4자리 숫자를 입력해주세요.')
+
+print(a_num)
+
